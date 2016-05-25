28c1673e695a91c902fa017c534a1111496015e9
jladau <jladau@gmail.com>
2016-05-24 18:12:06 -0700

    2016-05-24b

---
+++ RandomEnvironmentalSequences/README.md
@@ -0,0 +1,4 @@
+# RandomEnvironmentalSequences
+Code for generating random sequences using frequencies of bases and sequence lengths from a fasta file.
+
+For additional documentation, see the 'doc/index.html' file. Run the script 'RandomEnvironmentalSequencesTest.sh' to check that code is running properly. All source code is located the 'MicrobialDarkMatter' and 'Shared' directories at https://github.com/jladau/JavaSource.
---
+++ RandomEnvironmentalSequences/RandomEnvironmentalSequencesTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#loading local variables
+sDir=$HOME/Documents/Research/Java/Distribution/MicrobialDarkMatter/RandomEnvironmentalSequences
+sOutputPath=$sDir/test/Test.fasta
+sFastaPath=$sDir/test/Bac_uncultured_atmospheric.fasta
+iRandomSeed=1234
+
+#running analysis
+java -cp $sDir/bin/MicrobialDarkMatter.jar edu.ucsf.RandomEnvironmentalSequences.RandomEnvironmentalSequencesLauncher --sOutputPath=$sOutputPath --sFastaPath=$sFastaPath --iRandomSeed=$iRandomSeed
+
+#checking for differences
+echo 'Checking for differences from correct output...'
+sleep 1s
+diff $sDir/test/Test.fasta $sDir/test/Test.CorrectOutput.fasta
+diff $sDir/test/Test.probabilities-observed.csv $sDir/test/Test.CorrectOutput.probabilities-observed.csv
+diff $sDir/test/Test.probabilities-simulated.csv $sDir/test/Test.CorrectOutput.probabilities-simulated.csv
+echo 'Done.'
